7cd3b3f2a97048cec9c9f6c27e58134eb6437ed6
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 17:00:50 +0100

    rust: Reformat examples.

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -1,8 +1,5 @@
-use std::{io, error::Error};
-use std::thread;
-use tinkerforge::{ip_connection::IpConnection, 
-                  thermocouple_v2_bricklet::*};
-
+use std::{error::Error, io, thread};
+use tinkerforge::{ip_connection::IpConnection, thermocouple_v2_bricklet::*};
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -13,21 +10,21 @@ fn main() -> Result<(), Box<dyn Error>> {
     let t = ThermocoupleV2Bricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-    // Don't use device before ipcon is connected.
+                                          // Don't use device before ipcon is connected.
 
-     let temperature_receiver = t.get_temperature_callback_receiver();
+    let temperature_receiver = t.get_temperature_callback_receiver();
 
-        // Spawn thread to handle received callback messages. 
-        // This thread ends when the `t` object
-        // is dropped, so there is no need for manual cleanup.
-        thread::spawn(move || {
-            for temperature in temperature_receiver {           
-                		println!("Temperature: {} °C", temperature as f32 /100.0);
-            }
-        });
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `t` object
+    // is dropped, so there is no need for manual cleanup.
+    thread::spawn(move || {
+        for temperature in temperature_receiver {
+            println!("Temperature: {} °C", temperature as f32 / 100.0);
+        }
+    });
 
-		// Set period for temperature callback to 1s (1000ms) without a threshold.
-		t.set_temperature_callback_configuration(1000, false, 'x', 0, 0);
+    // Set period for temperature callback to 1s (1000ms) without a threshold.
+    t.set_temperature_callback_configuration(1000, false, 'x', 0, 0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
--- software/examples/rust/example_simple.rs
+++ software/examples/rust/example_simple.rs
@@ -1,8 +1,6 @@
-use std::{io, error::Error};
+use std::{error::Error, io};
 
-use tinkerforge::{ip_connection::IpConnection, 
-                  thermocouple_v2_bricklet::*};
-
+use tinkerforge::{ip_connection::IpConnection, thermocouple_v2_bricklet::*};
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -13,11 +11,11 @@ fn main() -> Result<(), Box<dyn Error>> {
     let t = ThermocoupleV2Bricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-    // Don't use device before ipcon is connected.
+                                          // Don't use device before ipcon is connected.
 
-		// Get current temperature.
-let temperature = t.get_temperature().recv()?;
-		println!("Temperature: {} °C", temperature as f32 /100.0);
+    // Get current temperature.
+    let temperature = t.get_temperature().recv()?;
+    println!("Temperature: {} °C", temperature as f32 / 100.0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
